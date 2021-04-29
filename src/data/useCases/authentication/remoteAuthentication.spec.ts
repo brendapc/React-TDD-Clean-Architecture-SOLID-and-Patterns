a0b875c6623f2a3011df6cda309bb40a9a7bd5de
Brenda Pereira <brendacamara@outlook.com>
2021-04-29 19:18:33 -0300

    refactor: use faker library to generate a random url to tests

--- src/data/useCases/authentication/remoteAuthentication.spec.ts
+++ src/data/useCases/authentication/remoteAuthentication.spec.ts
@@ -1,12 +1,13 @@
 import { HttpPostClientMock } from '../../test/mockHttpClient'
 import { RemoteAuthentication } from './remoteAuthentication'
+import faker from 'faker'
 
 type SutTypes = {
   sut: RemoteAuthentication
   httpPostClientMock: HttpPostClientMock
 }
 
-const mountSystemUnderTest = (url = 'anyUrl'): SutTypes => {
+const mountSystemUnderTest = (url: string = faker.internet.url()): SutTypes => {
   const httpPostClientMock = new HttpPostClientMock()
   const sut = new RemoteAuthentication(url, httpPostClientMock)
   return {
@@ -17,7 +18,7 @@ const mountSystemUnderTest = (url = 'anyUrl'): SutTypes => {
 
 describe('RemoteAuthentication', () => {
   test('should HttpClient with correct url', async () => {
-    const url = 'mockUrl'
+    const url = faker.internet.url()
     const { sut, httpPostClientMock } = mountSystemUnderTest(url)
     await sut.auth()
     expect(httpPostClientMock.url).toBe(url)
