1513ed576f6ae76a38b2a6f2f8ec4d1db8824afc
Brenda Pereira <brendacamara@outlook.com>
2021-05-01 13:43:59 -0300

    feat: add specific types to Typescript Generic Types

--- src/data/useCases/authentication/remoteAuthentication.ts
+++ src/data/useCases/authentication/remoteAuthentication.ts
@@ -3,11 +3,12 @@ import { HttpStatusCode } from '@/data/protocols/http/httpResponse'
 import { AuthenticationParams } from '@/domain/useCases/authentication'
 import { InvalidCredentialsError } from '@/domain/errors/invalidCredentialsError/InvalidCredentialsError'
 import { UnexpectedError } from './../../../domain/errors/unexpectedError/InvalidCredentialsError'
+import { IAccountModel } from './../../../domain/models/accountModel'
 
 export class RemoteAuthentication {
   constructor (
     private readonly url: string,
-    private readonly httpPostClient: IHttpPostClient
+    private readonly httpPostClient: IHttpPostClient<AuthenticationParams, IAccountModel>
   ) {}
 
   async auth (params: AuthenticationParams): Promise<void> {
