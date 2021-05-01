da13f0bd63bd8808266a1d2857d7e60f35bc9362
Brenda Pereira <brendacamara@outlook.com>
2021-05-01 14:02:09 -0300

    feat: ensure RemoteAuthentication returns an account on success

--- src/data/useCases/authentication/remoteAuthentication.ts
+++ src/data/useCases/authentication/remoteAuthentication.ts
@@ -1,17 +1,17 @@
 import { IHttpPostClient } from '@/data/protocols/http/httpPostClient'
 import { HttpStatusCode } from '@/data/protocols/http/httpResponse'
-import { AuthenticationParams } from '@/domain/useCases/authentication'
+import { AuthenticationParams, IAuthentication } from '@/domain/useCases/authentication'
 import { InvalidCredentialsError } from '@/domain/errors/invalidCredentialsError/InvalidCredentialsError'
-import { UnexpectedError } from './../../../domain/errors/unexpectedError/InvalidCredentialsError'
-import { IAccountModel } from './../../../domain/models/accountModel'
+import { UnexpectedError } from '@/domain/errors/unexpectedError/InvalidCredentialsError'
+import { IAccountModel } from '@/domain/models/accountModel'
 
-export class RemoteAuthentication {
+export class RemoteAuthentication implements IAuthentication {
   constructor (
     private readonly url: string,
     private readonly httpPostClient: IHttpPostClient<AuthenticationParams, IAccountModel>
   ) {}
 
-  async auth (params: AuthenticationParams): Promise<void> {
+  async auth (params: AuthenticationParams): Promise<IAccountModel> {
     const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
@@ -19,7 +19,7 @@ export class RemoteAuthentication {
     switch (httpResponse.statusCode) {
       case HttpStatusCode.unauthorized:
         throw new InvalidCredentialsError()
-      case HttpStatusCode.okRequest: break
+      case HttpStatusCode.okRequest: return httpResponse.body
 
       default: throw new UnexpectedError()
     }
