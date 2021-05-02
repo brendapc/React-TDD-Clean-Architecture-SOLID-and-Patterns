943be77b62fe6549cc3cf043b10588b263655588
Brenda Pereira <brendacamara@outlook.com>
2021-05-01 21:10:05 -0300

    feat: ensure AxiosHttpClient calls axios with correct verb

--- src/infra/http/axiosHttpClient/axiosHttpClient.ts
+++ src/infra/http/axiosHttpClient/axiosHttpClient.ts
@@ -2,6 +2,6 @@ import { IHttpPostParams } from '@/data/protocols/http'
 import axios from 'axios'
 export class AxiosHttpClient {
   async post (params: IHttpPostParams<any>): Promise<void> {
-    await axios(params.url)
+    await axios.post(params.url)
   }
 }
