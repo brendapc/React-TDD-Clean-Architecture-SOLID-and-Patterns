fc1b23566078ae93ebbf4dbe25509ad8f86dfef6
Brenda Pereira <brendacamara@outlook.com>
2021-05-01 21:02:38 -0300

    test: ensure AxiosHttpClient calls axios with correct url

---
+++ src/infra/http/axiosHttpClient/axiosHttpClient.spec.ts
@@ -0,0 +1,25 @@
+import { AxiosHttpClient } from './axiosHttpClient'
+import faker, { fake } from 'faker'
+import axios from 'axios'
+
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+interface SutTypes {
+  sut: AxiosHttpClient
+}
+const mountSystemUnderTest = (): SutTypes => {
+  const sut = new AxiosHttpClient()
+  return {
+    sut
+  }
+}
+
+describe('AxiosHttpClient', () => {
+  test('should call axios with correct url', async () => {
+    const url = faker.internet.url()
+    const { sut } = mountSystemUnderTest()
+    await sut.post({ url })
+    expect(mockedAxios).toHaveBeenCalledWith(url)
+  })
+})
