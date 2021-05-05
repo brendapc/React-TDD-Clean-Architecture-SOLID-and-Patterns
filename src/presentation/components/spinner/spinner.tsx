f6eb7108ce7223bb186ceb81226ac75e57eec293
Brenda Pereira <brendacamara@outlook.com>
2021-05-05 20:59:42 -0300

    feat: add spinner component

---
+++ src/presentation/components/spinner/spinner.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from './spinner-styles.scss'
+
+type Props = React.HTMLAttributes<HTMLElement>
+
+export const Spinner: React.FC<Props> = (props: Props) => {
+  return (
+    <div className={[Styles.spinner, props.className].join(' ')}>
+      <div/><div/><div/><div/><div/><div/><div/><div/><div/><div/><div/><div/>
+    </div>
+  )
+}
