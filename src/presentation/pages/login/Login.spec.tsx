4b760474ea426ba4f09415b8b5701be09f261116
Brenda Pereira <brendacamara@outlook.com>
2021-05-08 17:53:09 -0300

    test: ensure login page will not render spinner and error message on page load

--- src/presentation/pages/login/Login.spec.tsx
+++ src/presentation/pages/login/Login.spec.tsx
@@ -3,7 +3,9 @@ import { render } from '@testing-library/react'
 import { Login } from './Login'
 
 describe('Login compoenent', () => {
-  test('should ', () => {
-    render(<Login />)
+  test('should not render spinner', () => {
+    const { getByTestId } = render(<Login />)
+    const errorWrap = getByTestId('error-wrap')
+    expect(errorWrap.childElementCount).toBe(0)
   })
 })
