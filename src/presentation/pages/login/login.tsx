01a706e3a2cd08c87c97ac010a31eb7917ca8b1b
Brenda Pereira <brendacamara@outlook.com>
2021-05-08 10:12:30 -0300

    refactor: move footer and header to their own components

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,4 +1,5 @@
-import { Logo } from '@/presentation/components/logo/logo'
+import LoginHeader from '@/presentation/components/loginHeader/loginHeader'
+import Footer from '@/presentation/components/footer/footer'
 import { Spinner } from '@/presentation/components/spinner/spinner'
 import React from 'react'
 import Styles from './login-styles.scss'
@@ -6,10 +7,7 @@ import Styles from './login-styles.scss'
 export const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-      <header className={Styles.header}>
-        <Logo />
-        <h1>4Dev - Enquetes para programadores</h1>
-      </header>
+      <LoginHeader />
       <form className={Styles.form}>
         <h2>Login</h2>
         <div className={Styles.inputWrapper}>
@@ -26,9 +24,8 @@ export const Login: React.FC = () => {
           <Spinner className={Styles.spinner} />
           <span className={Styles.error}> error</span>
         </div>
-
       </form>
-      <footer className={Styles.footer}></footer>
+      <Footer />
     </div>
   )
 }
