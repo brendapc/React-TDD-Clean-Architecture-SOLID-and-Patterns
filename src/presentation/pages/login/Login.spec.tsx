a5edc82ad8269cb37196a70b70e049928793d586
Brenda Pereira <brendacamara@outlook.com>
2021-05-09 15:44:56 -0300

    test: ensure button starts disabled

--- src/presentation/pages/login/Login.spec.tsx
+++ src/presentation/pages/login/Login.spec.tsx
@@ -3,9 +3,11 @@ import { render } from '@testing-library/react'
 import { Login } from './Login'
 
 describe('Login compoenent', () => {
-  test('should not render spinner', () => {
+  test('should mount components with inital state', () => {
     const { getByTestId } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+    const submitButton = getByTestId('submit-button') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
   })
 })
