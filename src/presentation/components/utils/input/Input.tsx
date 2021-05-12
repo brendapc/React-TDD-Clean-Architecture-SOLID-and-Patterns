ba8e5a9c88a7ba817a87407d984a652579d786ec
Brenda Pereira <brendacamara@outlook.com>
2021-05-12 14:32:07 -0300

    feat: handle input change

--- src/presentation/components/utils/input/Input.tsx
+++ src/presentation/components/utils/input/Input.tsx
@@ -4,8 +4,8 @@ import FormContext from '../../../contexts/form/FormContext'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 export const Input: React.FC<Props> = (props: Props) => {
-  const { errorState } = useContext(FormContext)
-  const error = errorState[`${props.name}`]
+  const { formState, setFormState } = useContext(FormContext)
+  const error = formState[`${props.name}Error`]
 
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
@@ -15,9 +15,16 @@ export const Input: React.FC<Props> = (props: Props) => {
     return '❗'
   }
 
+  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
+    setFormState({
+      ...formState,
+      [event.target.name]: event.target.value
+    })
+  }
+
   return (
     <div className={Styles.inputWrapper}>
-      <input {...props} readOnly onFocus={enableInput} />
+      <input {...props} data-testid={props.name} readOnly onFocus={enableInput} onChange={handleChange} />
       <span data-testid={`${props.name}-status`} title={error} className={Styles.status}>{getStatus()}</span>
     </div>
   )
