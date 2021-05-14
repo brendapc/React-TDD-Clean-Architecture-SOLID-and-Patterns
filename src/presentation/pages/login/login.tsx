71a669259efc35f88ed4d85ba56c1055cd759b61
Brenda Pereira <brendacamara@outlook.com>
2021-05-14 09:25:35 -0300

    feat: ensure Login submit button is enabled if form values are valid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -35,7 +35,7 @@ export const Login: React.FC<Props> = ({ validation }: Props) => {
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
           <Input type="password" name="password" placeholder="Digite sua senha" />
-          <button data-testid="submit-button" className={Styles.submit} disabled type="submit">Entrar</button>
+          <button data-testid="submit-button" className={Styles.submit} disabled={!!formState.emailError || !!formState.passwordError} type="submit">Entrar</button>
           <span className={Styles.link}>Criar conta</span>
           <FormStatus />
         </form>
