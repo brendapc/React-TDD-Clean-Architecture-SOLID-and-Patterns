5e2e8c6a4eebfd2e6d70918b81c09215cc9c045f
Brenda Pereira <brendacamara@outlook.com>
2021-05-15 19:19:00 -0300

    refactor: create sut to test class

--- src/validation/validators/minLength/minLengthValidation.spec.ts
+++ src/validation/validators/minLength/minLengthValidation.spec.ts
@@ -1,16 +1,19 @@
 import { InvalidFieldError } from '@/validation/errors/invalidFieldError'
 import { MinLengthValidation } from './minLengthValidation'
+import faker from 'faker'
+
+const makeSystemUnderTest = (): MinLengthValidation => new MinLengthValidation(faker.database.column(), 5)
 
 describe('Minimum Length Validation', () => {
   test('should return error if value is invalid', () => {
-    const sut = new MinLengthValidation('field', 5)
+    const sut = makeSystemUnderTest()
     const error = sut.validate('123')
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('should not return an error if field is valid', () => {
-    const sut = new MinLengthValidation('field', 5)
-    const error = sut.validate('123')
+    const sut = makeSystemUnderTest()
+    const error = sut.validate('12345')
     expect(error).toBeFalsy()
   })
 })
