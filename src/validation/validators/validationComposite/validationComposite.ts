69bd252fb9539e4c83b377bbe9533e8f73096816
Brenda Pereira <brendacamara@outlook.com>
2021-05-15 19:39:25 -0300

    feat: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/validationComposite/validationComposite.ts
@@ -0,0 +1,15 @@
+import { FieldValidation } from '@/validation/protocols/fieldValidation'
+import { IValidation } from './../../../presentation/protocols/validation'
+
+export class ValidationComposite implements IValidation {
+  constructor (private readonly validators: FieldValidation[]) {}
+
+  validate (fieldName: string, fieldValue: string): string {
+    const fieldValidators = this.validators.filter(validator => validator.field === fieldName)
+    for (const validator of fieldValidators) {
+      const error = validator.validate(fieldValue)
+      if (error) return error.message
+    }
+    return null
+  }
+}
