ba20c4f9f45c41be5ff745dae60f2f80a80c9485
Brenda Pereira <brendacamara@outlook.com>
2021-05-15 09:05:04 -0300

    feat: ensure RequiredFieldValidation doesn't return an error if field is not empty

--- src/validation/requiredField/requiredFieldValidation.spec.ts
+++ src/validation/requiredField/requiredFieldValidation.spec.ts
@@ -1,10 +1,28 @@
 import { RequiredFieldError } from '../errors/requiredFieldError'
 import { RequiredFieldValidation } from './requiredFieldValidation'
+import faker from 'faker'
+
+interface SutTypes {
+  sut: RequiredFieldValidation
+}
+
+const makeSystemUnderTest = (field): SutTypes => {
+  const sut = new RequiredFieldValidation(field)
+  return {
+    sut
+  }
+}
 
 describe('Required Field Validation', () => {
   test('should return an error if ', () => {
-    const sut = new RequiredFieldValidation('email')
+    const { sut } = makeSystemUnderTest('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('should not return an error if field id not empty', () => {
+    const { sut } = makeSystemUnderTest('email')
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
