ca61a4f134ca5fa33650ab7c73be66a99c4cb626
Brenda Pereira <brendacamara@outlook.com>
2021-05-15 19:25:30 -0300

    refactor: emalValidator tests to return falsy if email field is empty

--- src/validation/validators/email/emailValidation.spec.ts
+++ src/validation/validators/email/emailValidation.spec.ts
@@ -6,22 +6,22 @@ interface SutTypes {
   sut: EmailValidation
 }
 
-const makeSystemUnderTest = (field): SutTypes => {
-  const sut = new EmailValidation(field)
-  return {
-    sut
-  }
-}
+const makeSystemUnderTest = (): EmailValidation => new EmailValidation(faker.database.column())
 
 describe('Email Validation', () => {
   test('should return error if email is invalid', () => {
-    const { sut } = makeSystemUnderTest(faker.random.word())
+    const sut = makeSystemUnderTest()
     const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
   })
   test('should not return an error if email is valid', () => {
-    const { sut } = makeSystemUnderTest(faker.random.word())
+    const sut = makeSystemUnderTest()
     const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
+  })
+  test('should return falsy if email is empty', () => {
+    const sut = makeSystemUnderTest()
+    const error = sut.validate('')
     expect(error).toBeFalsy()
   })
 })
