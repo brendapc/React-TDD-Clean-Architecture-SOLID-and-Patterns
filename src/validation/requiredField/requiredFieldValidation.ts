7a394f28dbf10cbc343bed9f0abe62e102d6b572
Brenda Pereira <brendacamara@outlook.com>
2021-05-15 08:59:29 -0300

    feat: ensure RequiredFieldValidation returns an error if field is empty

---
+++ src/validation/requiredField/requiredFieldValidation.ts
@@ -0,0 +1,9 @@
+import { FieldValidation } from '../protocols/fieldValidation'
+
+export class RequiredFieldValidation implements FieldValidation {
+  constructor (readonly field: string) {}
+
+  validate (value: string): Error {
+    return value ? null : new Error('Campo Obrigatório')
+  }
+}
