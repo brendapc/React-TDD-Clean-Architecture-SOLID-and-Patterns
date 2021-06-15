265b58a451ad010ec9bf5a12ff5c180618f42d4a
Brenda Pereira Camara <brendacamara@outlook.com>
2021-06-15 08:22:20 -0300

    feat: addded signup to router

--- src/presentation/components/router/Router.tsx
+++ src/presentation/components/router/Router.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import '@/presentation/styles/global.scss'
 import { makeLoginPage } from '@/main/factories/pages/login/loginPageFactory'
+import { Signup } from '@/presentation/pages/'
 
 type Props = {
   makeLoginPage: React.FC
@@ -12,6 +13,7 @@ export const Router: React.FC<Props> = ({ makeLoginPage }: Props) => {
     <BrowserRouter>
       <Switch>
         <Route path="/login" exact component={makeLoginPage} />
+        <Route path="/signup" exact component={Signup} />
       </Switch>
     </BrowserRouter>
   )
