13d8bc1b076472c3eb9bd4f9f937f5baeed91b9a
Brenda Pereira Camara <brendacamara@outlook.com>
2021-06-15 08:49:46 -0300

    feat: ensure Signup starts with correct initial state

--- src/presentation/pages/signup/Signup.tsx
+++ src/presentation/pages/signup/Signup.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useHistory } from 'react-router-dom'
 import { Footer, LoginHeader } from '@/presentation/components/layout'
 import { Input, FormStatus } from '@/presentation/components/utils'
@@ -6,17 +6,26 @@ import Context from '../../contexts/form/FormContext'
 import Styles from './signup-styles.scss'
 
 export const Signup: React.FC = () => {
+  const [formState] = useState({
+    isLoading: false,
+    usernameError: 'Campo Obrigatório',
+    emailError: 'Campo Obrigatório',
+    passwordError: 'Campo Obrigatório',
+    passwordConfirmationError: 'Campo Obrigatório',
+    mainError: ''
+  })
+
   return (
       <div className={Styles.signup}>
         <LoginHeader />
-        <Context.Provider value={{ formState: {} }}>
+        <Context.Provider value={{ formState }}>
           <form className={Styles.form} >
             <h2>Cadastro</h2>
-            <Input type="text" name="name" placeholder="Digite seu nome" />
+            <Input type="text" name="username" placeholder="Digite seu nome" />
             <Input type="email" name="email" placeholder="Digite seu e-mail" />
             <Input type="password" name="password" placeholder="Digite sua senha" />
-            <Input type="password" name="password" placeholder="Confirme sua senha" />
-            <button className={Styles.submit} >Entrar</button>
+            <Input type="password" name="passwordConfirmation" placeholder="Confirme sua senha" />
+            <button className={Styles.submit} data-testid="submit-button" disabled>Entrar</button>
             <Link to="/login" className={Styles.link}>Voltar para login</Link>
             <FormStatus />
           </form>
