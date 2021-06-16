5d19b53635af983ec4738e84c2131e420b9d25fc
Brenda Pereira Camara <brendacamara@outlook.com>
2021-06-16 08:57:28 -0300

    feat: ensure Signup prevents AddAccount to be called  more than once

--- src/presentation/mocks/addAccount.ts
+++ src/presentation/mocks/addAccount.ts
@@ -5,9 +5,11 @@ import { mockAccountModel } from '@/domain/mocks'
 export class AddAccountSpy implements IAddAccount {
   fakeAccount = mockAccountModel()
   params: IAddAccountParams
+  callsCount = 0
 
   async add (params: IAddAccountParams): Promise<IAccountModel> {
     this.params = params
+    this.callsCount++
     return this.fakeAccount
   }
 }
