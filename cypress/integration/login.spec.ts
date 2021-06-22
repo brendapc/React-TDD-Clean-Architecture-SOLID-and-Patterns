55b296267421076dd799d0bbf9e5614da0fb11a2
Brenda Pereira Camara <brendacamara@outlook.com>
2021-06-21 23:21:01 -0300

    test: e2e that login loads with correct elements and states

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -2,7 +2,11 @@ describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
   })
-  it('should load login', () => {
-    cy.getByTestId('email-status').should('have.attr', 'title', 'Campo Obrigatório')
+  it('should load login with correct initial state', () => {
+    cy.getByTestId('email-status').should('have.attr', 'title', 'Campo Obrigatório').should('contain.text', '❗')
+    cy.getByTestId('password-status').should('have.attr', 'title', 'Campo Obrigatório').should('contain.text', '❗')
+    cy.getByTestId('submit-button').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
+// ✔️
