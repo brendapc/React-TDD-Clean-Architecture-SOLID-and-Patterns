3de8fe8814a5d4319c0642a3697d6c3557ce9cb8
Brenda Pereira Camara <brendacamara@outlook.com>
2021-06-23 20:47:16 -0300

    feat: add animation to input style

--- src/presentation/components/utils/input/Input.tsx
+++ src/presentation/components/utils/input/Input.tsx
@@ -1,22 +1,15 @@
 import Styles from './input-styles.scss'
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import FormContext from '@/presentation/contexts/form/FormContext'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 export const Input: React.FC<Props> = (props: Props) => {
   const { formState, setFormState } = useContext(FormContext)
+  const inputRef = useRef<HTMLInputElement>()
   const error = formState[`${props.name}Error`]
 
   const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
     event.target.readOnly = false
-  }
-
-  const getStatus = (): string => {
-    return error ? '❗' : '✔️'
-  }
-
-  const getTitle = (): string => {
-    return error || 'Everything ok'
   }
 
   const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
@@ -28,8 +21,21 @@ export const Input: React.FC<Props> = (props: Props) => {
 
   return (
     <div className={Styles.inputWrapper}>
-      <input {...props} data-testid={props.name} readOnly onFocus={enableInput} onChange={handleChange} />
-      <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
+      <input
+        {...props}
+        ref={inputRef}
+        placeholder=" "
+        data-testid={props.name}
+        readOnly
+        onFocus={enableInput}
+        onChange={handleChange}
+      />
+      <label onClick={() => { inputRef.current.focus() }}>{props.placeholder}</label>
+      <span
+        data-testid={`${props.name}-status`}
+        title={error || 'Everything ok'}
+        className={Styles.status}>{error ? '❗' : '✔️'}
+      </span>
     </div>
   )
 }
