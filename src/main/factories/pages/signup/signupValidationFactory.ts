e2b22c2e7a071e819300003c513ddc609bab67b5
Brenda Pereira Camara <brendacamara@outlook.com>
2021-06-27 13:00:10 -0300

    test: ensure Signup presents error state if form is invalid

--- src/main/factories/pages/signup/signupValidationFactory.ts
+++ src/main/factories/pages/signup/signupValidationFactory.ts
@@ -2,7 +2,7 @@ import { ValidationBuilder, ValidationComposite } from '@/validation/validators'
 
 export const makeSignupValidation = (): ValidationComposite => {
   return ValidationComposite.build([
-    ...ValidationBuilder.field('username').required().build(),
+    ...ValidationBuilder.field('username').required().min(2).build(),
     ...ValidationBuilder.field('email').required().email().build(),
     ...ValidationBuilder.field('password').required().min(5).build(),
     ...ValidationBuilder.field('passwordConfirmation').required().sameAs('password').build()
