a232e135d5044f4a937a4baca56dd06f035af9a3
Brenda Pereira Camara <brendacamara@outlook.com>
2021-06-27 13:23:56 -0300

    fix: sign up factory imports

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from '@/presentation/pages'
 import { makeLoginPage } from './factories/pages/login/loginPageFactory'
-import { makeSignupPage } from './factories/pages/signup/loginPageFactory'
+import { makeSignupPage } from './factories/pages/signup/signupPageFactory'
 
 ReactDOM.render(
   <Router
