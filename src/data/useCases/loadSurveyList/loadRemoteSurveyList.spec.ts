d2a99ab20eef0def224be954e76ba5092af98b29
Brenda Pereira Camara <brendacamara@outlook.com>
2021-07-01 09:04:26 -0300

    test: ensure RemoteLoadSurveyList throws UnexpectedError on 404

--- src/data/useCases/loadSurveyList/loadRemoteSurveyList.spec.ts
+++ src/data/useCases/loadSurveyList/loadRemoteSurveyList.spec.ts
@@ -35,4 +35,13 @@ describe('Remote Load Survey List', () => {
     const promise = sut.loadAll()
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('should throw UnexpectedError if HttpGetClient returns 404', async () => {
+    const { sut, httpGetClientSpy } = makeSystemUnderTest()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
+    }
+    const promise = sut.loadAll()
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
