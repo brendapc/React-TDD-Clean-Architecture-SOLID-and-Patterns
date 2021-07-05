ec7eb02fecf5f0cb490b16026a0dd0cc120f09a0
Brenda Pereira Camara <brendacamara@outlook.com>
2021-07-05 16:34:09 -0300

    feat: create initial survey list view

--- src/presentation/components/router/Router.tsx
+++ src/presentation/components/router/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import '@/presentation/styles/global.scss'
 import { makeLoginPage } from '@/main/factories/pages/login/loginPageFactory'
 import { Signup } from '@/presentation/pages/'
+import { SurveyList } from '@/presentation/pages/surveyList/SurveyList'
 
 type Props = {
   makeLoginPage: React.FC
@@ -15,6 +16,7 @@ export const Router: React.FC<Props> = (factory: Props) => {
       <Switch>
         <Route path="/login" exact component={factory.makeLoginPage} />
         <Route path="/signup" exact component={factory.makeSignupPage} />
+        <Route path="/" exact component={SurveyList } />
       </Switch>
     </BrowserRouter>
   )
