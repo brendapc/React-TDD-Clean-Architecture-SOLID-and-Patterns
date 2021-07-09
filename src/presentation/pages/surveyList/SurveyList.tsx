6ac24f8aed7c0dd778490698e1220a6c34e677a7
Brenda Pereira Camara <brendacamara@outlook.com>
2021-07-08 21:02:48 -0300

    feat: add responsiveness to mobile size

--- src/presentation/pages/surveyList/SurveyList.tsx
+++ src/presentation/pages/surveyList/SurveyList.tsx
@@ -24,6 +24,48 @@ export const SurveyList: React.FC = () => {
                         </div>
                         <footer>Ver resultado</footer>
                     </li>
+                    <li>
+                        <div className={Styles.surveyContent}>
+                            <ThumbsIcons className={Styles.iconWrapper} iconName={IconName.thumbsDown} />
+                            <time>
+                                <span className={Styles.day}>05</span>
+                                <span className={Styles.month}>07</span>
+                                <span className={Styles.year}>2021</span>
+                            </time>
+                            <p>
+                                Qual é seu framework preferido?
+                            </p>
+                        </div>
+                        <footer>Ver resultado</footer>
+                    </li>
+                    <li>
+                        <div className={Styles.surveyContent}>
+                            <ThumbsIcons className={Styles.iconWrapper} iconName={IconName.thumbsDown} />
+                            <time>
+                                <span className={Styles.day}>05</span>
+                                <span className={Styles.month}>07</span>
+                                <span className={Styles.year}>2021</span>
+                            </time>
+                            <p>
+                                Qual é seu framework preferido?
+                            </p>
+                        </div>
+                        <footer>Ver resultado</footer>
+                    </li>
+                    <li>
+                        <div className={Styles.surveyContent}>
+                            <ThumbsIcons className={Styles.iconWrapper} iconName={IconName.thumbsDown} />
+                            <time>
+                                <span className={Styles.day}>05</span>
+                                <span className={Styles.month}>07</span>
+                                <span className={Styles.year}>2021</span>
+                            </time>
+                            <p>
+                                Qual é seu framework preferido?
+                            </p>
+                        </div>
+                        <footer>Ver resultado</footer>
+                    </li>
 
                 </ul>
             </div>
