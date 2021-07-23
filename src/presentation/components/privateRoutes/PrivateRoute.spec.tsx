c4134721254757d54b13e5a4d9e66c6209db10d7
Brenda Pereira <brenda.camara@eldorado.org.br>
2021-07-23 13:58:43 -0300

    refactor: move test sut to a factory method

--- src/presentation/components/privateRoutes/PrivateRoute.spec.tsx
+++ src/presentation/components/privateRoutes/PrivateRoute.spec.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import { Router } from 'react-router-dom'
 import { render } from '@testing-library/react'
-import { createMemoryHistory } from 'history'
+import { createMemoryHistory, MemoryHistory } from 'history'
 import { PrivateRoute } from './PrivateRoute'
 
-describe('PrivateRoute', () => {
-  test('should redirect to /login if token is empty', () => {
-    const history = createMemoryHistory({ initialEntries: ['/'] })
-    render(
+type SutTypes = {
+  history: MemoryHistory
+}
+
+const makeSystemUnderTest = (): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] })
+  render(
     <Router history={history}>
         <PrivateRoute />
     </Router>
-    )
+  )
+  return {
+    history
+  }
+}
+
+describe('PrivateRoute', () => {
+  test('should redirect to /login if token is empty', () => {
+    const { history } = makeSystemUnderTest()
     expect(history.location.pathname).toBe('/login')
   })
 })
