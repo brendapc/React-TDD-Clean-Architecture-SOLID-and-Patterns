5dde5dc1084af7a721262d001ddaf5bce3eb16cf
Brenda Pereira <brenda.camara@eldorado.org.br>
2021-07-23 13:37:37 -0300

    test:  the GET usecase of LocalStorageAdapter

--- src/infra/cache/localStorageAdapter/localStorageAdapter.spec.ts
+++ src/infra/cache/localStorageAdapter/localStorageAdapter.spec.ts
@@ -12,11 +12,25 @@ describe('LocalStorageAdapter', () => {
     const sut = new LocalStorageAdapter()
     return sut
   }
-  test('should call localStorage with correct values', async () => {
-    const sut = makeSystemUnderTest()
-    const key = faker.database.column()
-    const value = faker.random.objectElement<IAccountModel>()
-    sut.set(key, value)
-    expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
+  describe('SET', () => {
+    test('should call localStorage.setItem with correct values', async () => {
+      const sut = makeSystemUnderTest()
+      const key = faker.database.column()
+      const value = faker.random.objectElement<IAccountModel>()
+      sut.set(key, value)
+      expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
+    })
+  })
+
+  describe('GET', () => {
+    test('should call localStorage.getItem with correct value', async () => {
+      const sut = makeSystemUnderTest()
+      const key = faker.database.column()
+      const value = faker.random.objectElement<IAccountModel>()
+      const getItemSpy = jest.spyOn(localStorage, 'getItem').mockReturnValueOnce(JSON.stringify(value))
+      const object = sut.get(key)
+      expect(object).toEqual(value)
+      expect(getItemSpy).toHaveBeenCalledWith(key)
+    })
   })
 })
