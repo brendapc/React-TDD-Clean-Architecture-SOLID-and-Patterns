085cbc220533f87b86387f6323f94dd7f44b839b
Brenda Pereira <brenda.camara@eldorado.org.br>
2021-07-23 13:25:00 -0300

    feat: add getCurrentAccount to ApiContext

--- src/presentation/contexts/api/ApiContext.tsx
+++ src/presentation/contexts/api/ApiContext.tsx
@@ -2,7 +2,8 @@ import { createContext } from 'react'
 import { IAccountModel } from '@/domain/models'
 
 type Props = {
-  setCurrentAccount: (account: IAccountModel) => void
+  setCurrentAccount?: (account: IAccountModel) => void
+  getCurrentAccount?: () => IAccountModel
 }
 
 export default createContext<Props>(null)
