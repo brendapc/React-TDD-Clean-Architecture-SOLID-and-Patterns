95599ce50ceb963d1d436ce0214609cbeb5b9ed2
Brenda Pereira <brenda.camara@eldorado.org.br>
2021-07-23 13:47:43 -0300

    test: get usecase of CurrentAccountAdapter

--- src/main/adapters/currentAccountAdapter.spec.ts
+++ src/main/adapters/currentAccountAdapter.spec.ts
@@ -1,22 +1,33 @@
 import { UnexpectedError } from '@/domain/errors'
 import { mockAccountModel } from '@/domain/mocks'
 import { LocalStorageAdapter } from '@/infra/cache/localStorageAdapter/localStorageAdapter'
-import { setCurrentAccountAdapter } from './currentAccountAdapter'
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from './currentAccountAdapter'
 
 jest.mock('@/infra/cache/localStorageAdapter/localStorageAdapter')
 
 describe('CurrentAccountAdapter', () => {
-  test('should call LocalStorageAdapter', () => {
-    const account = mockAccountModel()
-    const setAccountSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set') // spy all instances on method set
-    setCurrentAccountAdapter(account) // run function
-    expect(setAccountSpy).toHaveBeenCalledWith('account', account)
-  })
+  describe('SET', () => {
+    test('should call LocalStorageAdapter.set with correct values', () => {
+      const fakeAccount = mockAccountModel()
+      const setAccountSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set') // spy all instances on method set
+      setCurrentAccountAdapter(fakeAccount) // run function
+      expect(setAccountSpy).toHaveBeenCalledWith('account', fakeAccount)
+    })
 
-  test('should throw unexpected error if received value is invalid', () => {
-    // Test if this function returns an exception
-    expect(() => {
-      setCurrentAccountAdapter(undefined)
-    }).toThrow(new UnexpectedError())
+    test('should throw unexpected error if received value is invalid', () => {
+      // Test if this function returns an exception
+      expect(() => {
+        setCurrentAccountAdapter(undefined)
+      }).toThrow(new UnexpectedError())
+    })
+  })
+  describe('GET', () => {
+    test('should call LocalStorageAdapter.get with correct value', () => {
+      const fakeAccount = mockAccountModel()
+      const getAccountSpy = jest.spyOn(LocalStorageAdapter.prototype, 'get').mockReturnValueOnce(fakeAccount)
+      const account = getCurrentAccountAdapter()
+      expect(getAccountSpy).toHaveBeenCalledWith('account')
+      expect(account).toEqual(fakeAccount)
+    })
   })
 })
