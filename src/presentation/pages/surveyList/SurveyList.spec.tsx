e5b1bb86e5ecd5573ac38a519cce23a4697a3bba
Brenda Pereira <brenda.camara@eldorado.org.br>
2021-07-26 11:53:49 -0300

    test: ensure LoadSurveyList is called one time

--- src/presentation/pages/surveyList/SurveyList.spec.tsx
+++ src/presentation/pages/surveyList/SurveyList.spec.tsx
@@ -1,17 +1,38 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { SurveyList } from './SurveyList'
+import { ISurveyModel } from '@/domain/models'
+import { ILoadSurveyList } from '@/domain/useCases'
 
-const makeSystemUnderTest= (): void => {
-  render(<SurveyList />)
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveyListSpy
+}
+
+class LoadSurveyListSpy implements ILoadSurveyList {
+  callsCount = 0
+  async loadAll (): Promise<ISurveyModel[]> {
+    this.callsCount++
+    return []
+  }
+}
+
+const makeSystemUnderTest = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy()
+  render(<SurveyList loadSurveyList={loadSurveyListSpy}/>)
+  return {
+    loadSurveyListSpy
+  }
 }
 
 describe('SurveyList Component', () => {
   test('should present 4 empty items on start', () => {
-      makeSystemUnderTest()
-      const surveyList = screen.getByTestId('survey-list')
-      expect(surveyList.querySelectorAll("li:empty").length).toBe(4)
+    makeSystemUnderTest()
+    const surveyList = screen.getByTestId('survey-list')
+    expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
   })
-  
-})
 
+  test('should call LoadSurveyList', () => {
+    const { loadSurveyListSpy } = makeSystemUnderTest()
+    expect(loadSurveyListSpy.callsCount).toBe(1)
+  })
+})
