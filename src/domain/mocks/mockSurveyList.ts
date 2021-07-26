a0ee62fa7e7c61f8bcdb5de6eee053ffe58b8ad6
Brenda Pereira <brenda.camara@eldorado.org.br>
2021-07-26 13:32:22 -0300

    test: ensure thumbsUp icon is shown if survey has been answered

--- src/domain/mocks/mockSurveyList.ts
+++ src/domain/mocks/mockSurveyList.ts
@@ -1,15 +1,25 @@
-import faker, { fake } from 'faker'
-import { ISurveyModel } from '../models'
+import faker, { fake } from "faker";
+import { ISurveyModel } from "../models";
 
-export const mockSurveyListModel = (): ISurveyModel[] => ([{
+export const mockSurveyModel = (): ISurveyModel => ({
   id: faker.datatype.uuid(),
   question: faker.random.words(10),
-  answers: [{
-    answer: faker.random.words(4),
-    image: faker.internet.url()
-  },{
-    answer: faker.random.words(5)
-  }],
+  answers: [
+    {
+      answer: faker.random.words(4),
+      image: faker.internet.url(),
+    },
+    {
+      answer: faker.random.words(5),
+    },
+  ],
   didAnswer: faker.datatype.boolean(),
-  date: faker.date.recent()
-}])
+  date: faker.date.recent(),
+});
+
+export const mockSurveyListModel = (): ISurveyModel[] => [
+  mockSurveyModel(),
+  mockSurveyModel(),
+  mockSurveyModel(),
+  mockSurveyModel(),
+];
