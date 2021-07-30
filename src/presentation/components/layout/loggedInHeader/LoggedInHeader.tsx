8a484cf7ddb686630e51296e6959e63ffa3583ca
Brenda Pereira <brenda.camara@eldorado.org.br>
2021-07-30 15:01:52 -0300

    feat: ensure logout calls setCurrentAccount with undefined

--- src/presentation/components/layout/loggedInHeader/LoggedInHeader.tsx
+++ src/presentation/components/layout/loggedInHeader/LoggedInHeader.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import { ApiContext } from '@/presentation/contexts'
+import React, { useContext } from 'react'
+import { useHistory } from 'react-router-dom'
 import { Logo } from '..'
 import Styles from './loggedInHeader.styles.scss'
 
 export const LoggedInHeader: React.FC = () => {
+  const { setCurrentAccount } = useContext(ApiContext)
+  const history = useHistory()
+  const logout = (event: any): void => {
+    event.preventDefault()
+    setCurrentAccount(undefined)
+    history.replace('/login')
+  }
+
   return (
     <header className={Styles.headerWrapper}>
     <div className={Styles.headerContent}>
         <Logo />
         <div className={Styles.logoutWrapper}>
             <span>Brenda</span>
-            <a href="#">Sair</a>
+            <a data-testid="logout" onClick={logout} href="#">Sair</a>
         </div>
     </div>
 </header>
