a8bc221d6f49fba1eb879dc4f49b805d8787216b
Brenda Pereira <brenda.camara@eldorado.org.br>
2021-08-02 14:46:58 -0300

    test: ensure SurveyList logout on logout link click

--- src/main/test/cypress/integration/surveyList.spec.ts
+++ src/main/test/cypress/integration/surveyList.spec.ts
@@ -25,4 +25,11 @@ describe('SurveyList', () => {
     const account = Helper.getLocalStorageItem('account')
     cy.getByTestId('username').should('contain.text', account.name)
   })
+
+  it('should logout properly on button click', () => {
+    Http.mockUnexpectedError()
+    cy.visit('')
+    cy.getByTestId('logout').click()
+    Helper.testUrl('/login')
+  })
 })
