72123e7190857d87448e759e8456cdb10803a971
Brenda Pereira <brenda.camara@eldorado.org.br>
2021-09-23 09:41:44 -0300

    test: ensure SurveyResult loads with correct initial state

--- src/presentation/pages/surveyResult/surveyResult.spec.tsx
+++ src/presentation/pages/surveyResult/surveyResult.spec.tsx
@@ -1,6 +1,17 @@
+import React from 'react'
+import { screen, render } from '@testing-library/react'
+import { ApiContext } from "@/presentation/contexts"
+import { mockAccountModel } from '@/domain/mocks'
+import { SurveyResult } from './SurveyResult'
+
 describe('Survey Result', () => {
-    test('should ', () => {
-        
+    test('should present correct initial state', () => {
+        render(
+            <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
+                <SurveyResult />
+            </ApiContext.Provider>
+        )
+        const surveyResult = screen.getByTestId('survey-result')
+        expect(surveyResult.childElementCount).toBe(0)
     })
-    
 })
